fa68266a938faf24bf7685503429fbd8279a6992
Lukas Alexandre <lukas@codelogic.me>
2016-12-30 22:33:00 +0100

    Adding and editing some TODOs

--- todos.rb
+++ todos.rb
@@ -1,10 +1,13 @@
-# TODO: testing
+# TODO: testings
 # description blablablabla
 def my_method
 
 end
 
-# TODO: A random todo
+# TODO: A random todo, modified
 def second_method
 
 end
+
+# TODO: second test
+{ asd: 123 }.freeze
