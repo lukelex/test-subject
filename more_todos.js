d1048aa994d140ef6ea7feb2f2bfbfa426db3a46
Lukas Alexandre <lukas@codelogic.me>
2017-05-29 15:20:20 -0300

    Adding a more complex todo in JS

---
+++ more_todos.js
@@ -0,0 +1,9 @@
+// TODO: multiline bitch!!
+// Write a regex for this!
+// I double dare you!!!
+function() { }
+
+// TODO: mooore multiline bitch!!
+// Write a more complex regex for this!
+// I triple dare you!!!
+function() { }
